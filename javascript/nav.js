bd7eeb5ff3d6edff4543356e3590137d8e57c453
takuma10Mi <amaebilove@gmail.com>
2020-08-28 22:43:02 +0900

    fuwa.js

--- javascript/nav.js
+++ javascript/nav.js
@@ -132,3 +132,14 @@ document.onkeydown = (e) => {
 // setTimeout(() => {
 //  rightSlide();
 // }, 500);
+
+$(window).scroll(function () {
+  $(".fadein").each(function () {
+    var elemPos = $(this).offset().top,
+      scroll = $(window).scrollTop(),
+      windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight + 100) {
+      $(this).addClass("scrollin");
+    }
+  });
+});
